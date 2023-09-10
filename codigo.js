1db25a518c0a208c14385ff68bacc74898889b5e
Mauricio-25 <mau252004@gmail.com>
2023-09-09 21:42:21 -0500

    Terminado la pantalla del desarrollador y scrum master

--- codigo.js
+++ codigo.js
@@ -1,74 +1,95 @@
-/*
-let barraLateral = document.querySelector(".barraLateral");
 
-let btnAbrirMenu = document.querySelector(".btnAbrirMenu");
+// ! ABRIR EL COMBOBOX
 
-btnAbrirMenu.addEventListener("click", ()=>{
-    barraLateral.classList.toggle("traslacion");
-})
-*/
 
 let comboBox = [];
 comboBox = document.querySelectorAll(".mensaje__combobox-contenedor");
 
-for(let i=0; i<comboBox.length; i++) {
-    let elemento = comboBox[i];
-    elemento.addEventListener("click", function(){
-        let opciones = elemento.nextElementSibling;
+
+let opciones = [];
+opciones = document.querySelectorAll(".mensaje__combobox-opcion");
+
+
+
+function abrirLosComboBox() {
+    comboBox = document.querySelectorAll(".mensaje__combobox-contenedor");
+
+    for(let i=0; i<comboBox.length; i++) {
+        let elemento = comboBox[i];
+        elemento.addEventListener("click", function(){
+            let opciones = elemento.nextElementSibling;
+            
+            if (!elemento.classList.contains("mensaje__combobox-disabled")) {
+                if (opciones.style.display == "block") {
+                    opciones.style.display = "none";
+                    setTimeout(() => {
+                        opciones.style.opacity = "0";
+                    }, 100);
+                }
         
-        if (!elemento.classList.contains("mensaje__combobox-disabled")) {
-            if (opciones.style.display == "block") {
+                else {
+                    opciones.style.display = "block";
+                    setTimeout(() => {
+                        opciones.style.opacity = "1";
+                    }, 100);
+                }
+            }
+        })
+    }
+}
+
+abrirLosComboBox();
+
+
+
+// ! Cerrar el comboBox cuando se haga click fuera de él
+
+function cerrarLosComboBox() {
+    comboBox = document.querySelectorAll(".mensaje__combobox-contenedor");
+
+    document.addEventListener("mousedown", (event)=>{
+        for(let i=0; i<comboBox.length; i++) {
+    
+            if(!comboBox[i].contains(event.target) && !comboBox[i].nextElementSibling.contains(event.target)){
+                let opciones = comboBox[i].nextElementSibling;
                 opciones.style.display = "none";
                 setTimeout(() => {
                     opciones.style.opacity = "0";
                 }, 100);
             }
-    
-            else {
-                opciones.style.display = "block";
-                setTimeout(() => {
-                    opciones.style.opacity = "1";
-                }, 100);
-            }
         }
-    })
+    });
 }
 
-// ! Cerrar el comboBox cuando se haga click fuera de él
+cerrarLosComboBox();
 
-document.addEventListener("mousedown", (event)=>{
-    for(let i=0; i<comboBox.length; i++) {
 
-        if(!comboBox[i].contains(event.target) && !comboBox[i].nextElementSibling.contains(event.target)){
-            let opciones = comboBox[i].nextElementSibling;
-            opciones.style.display = "none";
-            setTimeout(() => {
-                opciones.style.opacity = "0";
-            }, 100);
-        }
-    }
-});
 
 // ! Escribir la opcion en el comboBox
-let opciones = [];
-opciones = document.querySelectorAll(".mensaje__combobox-opcion");
 
-for(let i=0; i<opciones.length; i++) {
-    let opcion = opciones[i];
-    opcion.addEventListener("click", function(){
+function escribirLosComboBox() {
+    opciones = document.querySelectorAll(".mensaje__combobox-opcion");
 
-        let texto = opcion.innerHTML;
-        let input = opcion.parentNode.previousElementSibling.querySelector(".mensaje__combobox-input");
-        let contenedor = opcion.parentNode;
+    for(let i=0; i<opciones.length; i++) {
+        let opcion = opciones[i];
+        opcion.addEventListener("click", function(){
 
-        input.value = texto;
+            let texto = opcion.innerHTML;
+            let input = opcion.parentNode.previousElementSibling.querySelector(".mensaje__combobox-input");
+            let contenedor = opcion.parentNode;
 
-        contenedor.style.display = "none";
-        setTimeout(() => {
-            contenedor.style.opacity = "0";
-        }, 100);
-    })
+            input.value = texto;
+
+            contenedor.style.display = "none";
+            setTimeout(() => {
+                contenedor.style.opacity = "0";
+            }, 100);
+        })
+    }
 }
+
+escribirLosComboBox();
+
 
 
 // ! cerrar la ventana
@@ -93,7 +114,7 @@ for(let i=0; i<btnCerrar.length; i++) {
 }
 
 
-// ! 
+// ! ABRIR VENTANA NUEVO
 
 let ventanaNuevo = document.querySelector(".mensaje-nuevo")
 
@@ -107,15 +128,158 @@ function abrirVentanaNuevo() {
 }
 
 
-// ! 
+// ! ABRIR VENTANA VER
 
-let ventanaVerTarea = document.querySelector(".mensaje-verTarea")
+let ventanaVer = document.querySelector(".mensaje-ver")
 
-function abrirVentanaVerTarea() {
+function abrirVentanaVer() {
     
-    ventanaVerTarea.style.display = "flex";
+    ventanaVer.style.display = "flex";
 
     setTimeout(() => {
-        ventanaVerTarea.style.opacity = "1";
+        ventanaVer.style.opacity = "1";
     }, 100);
 }
+
+
+
+// ! ABRIR VENTANA DE EDITAR 
+
+let ventanaEditar = document.querySelector(".mensaje-editar")
+
+function abrirVentanaEditar() {
+    
+    ventanaEditar.style.display = "flex";
+
+    setTimeout(() => {
+        ventanaEditar.style.opacity = "1";
+    }, 100);
+}
+
+
+
+// ! ABRIR VENTANA DE ELIMINAR 
+
+let ventanaEliminar = document.querySelector(".mensaje-eliminar")
+
+function abrirVentanaEliminar() {
+    
+    ventanaEliminar.style.display = "flex";
+
+    setTimeout(() => {
+        ventanaEliminar.style.opacity = "1";
+    }, 100);
+}
+
+
+
+
+
+// ! BUSCADOR
+
+let buscador = [];
+buscador = document.querySelectorAll(".buscador");
+
+function añadirLosBuscadores() {
+    buscador = document.querySelectorAll(".buscador");
+
+    for (i=0; i<buscador.length; i++) {
+        let input = buscador[i];
+        input.addEventListener("keyup",(e)=>{
+            let lista = input.parentNode.nextElementSibling;
+            //let encontrado = false;
+            let texto = input.value.toUpperCase();
+        
+            for (i=0; i<lista.children.length; i++){
+                if(lista.children[i].innerHTML.toUpperCase().includes(texto)) {
+                lista.children[i].style.display = "inline-block";
+                encontrado = true;
+                } else {
+                lista.children[i].style.display = "none";
+                }
+            }
+            
+            /*
+            if(!encontrado) {
+                lista.children[lista.children.length-1].style.display = "flex";
+            } else {
+                lista.children[lista.children.length-1].style.display = "none";
+            }
+            */
+        });
+    }
+}
+
+añadirLosBuscadores();
+
+
+// ! ELIMINAR DESARROLLADOR
+
+function eliminarDesarrollador(boton) {
+    const desarrolladores = boton.parentNode.parentNode;
+
+    if (desarrolladores.childElementCount > 1) {
+        const desarrollador = boton.parentNode;
+
+        desarrolladores.removeChild(desarrollador);
+    }
+}
+
+// ! AGREGAR DESARROLLADOR
+
+function añadirDesarrollador(boton) {
+    const desarrolladores = boton.previousElementSibling;
+
+    const desarrollador = `
+        <div class="desarrolladores__desarrollador">
+            <input class="mensaje__input" type="text" name="desarrolladores[]">
+            <div class="desarrolladores__icono" onclick="eliminarDesarrollador(this)">
+                <i class="fa-solid fa-trash-can"></i>
+            </div>
+        </div>
+    `;
+
+    desarrolladores.insertAdjacentHTML('beforeend', desarrollador);
+
+}
+
+
+// ! AGREGAR DESARROLLADOR Y BUSCADOR
+
+function añadirDesarrolladorBuscador(boton) {
+    const desarrolladores = boton.previousElementSibling;
+
+    //let opciones = desarrolladores.querySelector(".mensaje__combobox-opciones").innerHTML;
+
+    const desarrollador = `
+        <div class="desarrolladores__desarrollador">
+            <div class="mensaje__combobox">
+                <div class="mensaje__input mensaje__combobox-contenedor" >
+                    <input type="text" class="mensaje__combobox-input buscador" placeholder="-- Selecciona --" name="desarrolladores[]">
+                </div>
+                <div class="mensaje__combobox-opciones">
+                    <div class="mensaje__combobox-opcion">Mauricio Soto Buenaño</div>
+                    <div class="mensaje__combobox-opcion">Carlos Santiago Guzman Vasquez</div>
+                    <div class="mensaje__combobox-opcion">Aaron Gabriel Pereda Saldaña</div>
+                    <div class="mensaje__combobox-opcion">Pedro Nicolas Suarez Vera</div>
+                    <div class="mensaje__combobox-opcion">Josué Edú Cordova Ypanaque</div>
+                </div>
+            </div>
+            <div class="desarrolladores__icono" onclick="eliminarDesarrollador(this)">
+                <i class="fa-solid fa-trash-can"></i>
+            </div>
+        </div>
+    `;
+
+    desarrolladores.insertAdjacentHTML('beforeend', desarrollador);
+
+    abrirLosComboBox();
+    cerrarLosComboBox();
+    añadirLosBuscadores();
+    escribirLosComboBox();
+    añadirLosBuscadores();
+
+}
+
+
+
